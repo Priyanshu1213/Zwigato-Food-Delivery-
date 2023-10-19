9a4ae2a5771146ad38d4f532911255d09fd4cb7c
Priyanshu1213 <priyanshuyadav39976@gmail.com>
2023-10-20 03:23:00 +0530

    third committ

--- frontend/src/Component/ViewPage/Filtter/Filtter.js
+++ frontend/src/Component/ViewPage/Filtter/Filtter.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import "./Filtter.css"
-export default function Filtter() {
+
+export default function Filtter(props) {
+
+
+  const handleCategoryChange = (category) => {
+    props.parentCallback(category);
+    
+  };
+  
+  
+
   return (
    <>
     <div className='Filtter_1'>
@@ -8,24 +18,24 @@ export default function Filtter() {
         <h1>Eat What Makes you Happy</h1>
         </div>
         <div className='Filtter_12'>
-        <div className='Filtter_121'>
-          <img src='https://i0.wp.com/www.thursdaynightpizza.com/wp-content/uploads/2022/06/veggie-pizza-side-view-out-of-oven.png?resize=720%2C480&ssl=1'/>
+        <div className='Filtter_121' onClick={() => handleCategoryChange('Pizza')}>
+          <img alt='Pizza' src='https://i0.wp.com/www.thursdaynightpizza.com/wp-content/uploads/2022/06/veggie-pizza-side-view-out-of-oven.png?resize=720%2C480&ssl=1'/>
           <h1>Pizza</h1>
         </div>
-        <div className='Filtter_121'>
-        <img src='https://images.zaiqathespicestore.com/uploads/2020/01/memonibiryani.png'/>
-        <h1>Briyani</h1>
+        <div className='Filtter_121' onClick={() => handleCategoryChange('Biryani')}>
+        <img alt='Pizza' src='https://img-global.cpcdn.com/recipes/d80b825db82e1dfe/680x482cq70/sindhi-chicken-biryani-recipe-main-photo.jpg'/>
+        <h1>Biryani</h1>
         </div>
-        <div className='Filtter_121'>
-        <img src='https://www.finedininglovers.com/sites/g/files/xknfdk626/files/2022-05/Popl_burger.jpg'/>
+        <div className='Filtter_121'onClick={() => handleCategoryChange('Burger')}>
+        <img alt='Pizza' src='https://www.finedininglovers.com/sites/g/files/xknfdk626/files/2022-05/Popl_burger.jpg'/>
           <h1>Burger</h1>
         </div>
-        <div className='Filtter_121'>
-          <img src='http://www.flavorquotient.com/wp-content/uploads/2016/08/Kadai-Chicken-FQ-5-1-of-1.jpg'/>
+        <div className='Filtter_121'onClick={() => handleCategoryChange('Chicken')}>
+          <img alt='Pizza' src='http://www.flavorquotient.com/wp-content/uploads/2016/08/Kadai-Chicken-FQ-5-1-of-1.jpg'/>
           <h1>Chicken</h1>
           </div>
-        <div className='Filtter_121'>
-        <img src='https://lh3.googleusercontent.com/asLohKLq0Q6JYKK6Crx9dNYY7bDhez4ePuUXUHJnuJbpifV2hD7HMrZ1XZekmVc9IXXZbyga6OY8G-DRUg51lCWXIQ=w500-rw'/>
+        <div className='Filtter_121'onClick={() => handleCategoryChange('Thali')}>
+        <img alt='Pizza' src='https://lh3.googleusercontent.com/asLohKLq0Q6JYKK6Crx9dNYY7bDhez4ePuUXUHJnuJbpifV2hD7HMrZ1XZekmVc9IXXZbyga6OY8G-DRUg51lCWXIQ=w500-rw'/>
           <h1>Thali</h1>
         </div>
         </div>
--- frontend/src/Component/ViewPage/MapforCartlog.js
+++ frontend/src/Component/ViewPage/MapforCartlog.js
@@ -2,16 +2,25 @@ import React, { useEffect, useState } from 'react'
 import Cartlog from './Cartlog/Cartlog';
 import axios from "axios"
 import "./MapforCartlog.css"
+import Filtter from './Filtter/Filtter';
 
 export default function MapforCartlog() {
+  
+    const [data,setData]= useState([]);
+    const [products, setProducts] = useState([]);
+    const [searchVal, setSearchVal] = useState("");
 
-    const [data,setData]=useState([]);
+    const [filt,setFilt]= useState("");
+
 
     useEffect(()=>{
   const temp=async ()=>{
     try {
         const fetcheddata=(await axios.get('http://localhost:5000/api/getdetails')).data
         setData(fetcheddata.response);
+        setProducts(fetcheddata.response)
+     
+        
     } catch (error) {
         console.log(error)
     }
@@ -21,13 +30,79 @@ export default function MapforCartlog() {
 
     },[])
     console.log(data)
+    
+    
+    const handleCallback = (childData ) => {
+      setFilt(childData)
+      const filtter = products.filter((item) => item.foodcategary.toLowerCase().includes(filt.toLowerCase()));
+      setData(filtter)
+
+    }
+    console.log(filt)
+    
+
+
+    // function handleSearchClick() {
+    //   if (searchVal === '') {
+    //     setData(products); // Reset to show all data
+    //   } else {
+    //     const filterBySearch = products.filter((item) => item.foodname.toLowerCase().includes(searchVal.toLowerCase()));
+    //     setData(filterBySearch);
+    //   }
+    // };
+    // console.log(products)
+    
+    
+  const filterBySearch = (event) => {
+    const query =event.target.value;
+    setSearchVal(query)
+    
+    if(query ===''&&filt===''){
+      setData(products)
+    }
+    else{
+      const filterlist = products.filter((item) => item.foodname.toLowerCase().includes(searchVal.toLowerCase()) );
+      
+      setData(filterlist);
+      
+    }
+    
+  };
+  console.log(products)
+  
+  
+  
   return (
     <>
-    <div className='Map1'>
-    {data.map(item=>
-      <Cartlog   priya={item}/>
-      )}
-    </div>
+
+    {/* <div className="search-header">
+                <input type='search' placeholder='Search...' onChange={e => setSearchVal(e.target.value)}>
+                </input>
+                <p  onClick={handleSearchClick}>Q</p>
+                
+            </div> */}
+
+
+<Filtter parentCallback={handleCallback}/>
+
+
+<div className="search-header">
+        
+        <input type='search' placeholder='Search...' id="search-box" value={searchVal} onChange={filterBySearch} />
+      </div>
+
+            
+      {data.length<=0 ?
+      ( <h1>No Data Found</h1> ):
+      (
+           <div className='Map1'>
+           {data.map(item=>
+           <Cartlog   priya={item} />
+           )}
+           </div>
+      )
+      }      
+    
 
     </>
   )
